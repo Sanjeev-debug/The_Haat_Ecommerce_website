effd533b2dc2dd4dd4e91e3943387107283d9323
Sanjeev <ms.saisanjeev@gmail.com>
2026-04-16 11:06:35 +0530

    fix path case issue

--- Frontend/src/routers/Layout.jsx
+++ Frontend/src/routers/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from '../components/header/header'
-import Footer from '../components/footer/Footer'
+import Header from '../components/header/Header.jsx'
+import Footer from '../components/footer/Footer.jsx'
 
 const Layout = () => {
   return (
